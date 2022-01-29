13a868588ce24071038c59416be079a2de35e752
Simon Calle Laverde <kyverde88@gmail.com>
2022-01-29 15:01:45 -0500

    SSG vs SSR.
    - Static Site Generation:
    º Pre-rendering.
    º Generating all the HTML at 'build' time.
    º (Most well suited for data that doesn't change often, and for sites that have a relatively low number of total pages. I.e a Blog).
    º Simulating 'data-base' or 'API' with Json files within "public" directory.
    º Implementing "getStaticProps" function.
    º Importing "Head" component. Anything inside this component will be rendered out to the <head></head> of the document.
    º Implementing "getStaticPaths" function.
    
    -- Vid 10:25.

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,11 +1,56 @@
-import { useRouter } from "next/router"
+// Car (Dynamic) Component
 
-export default function Car() {
+import { useRouter } from "next/router"
+import Head from "next/head"
+
+// Destructuring car 'prop' to use in car component
+export default function Car({ car }) {
 	const router = useRouter()
 	const { id } = router.query
 
-	return <div>
-		<h1>This is the dynamic car component for:</h1>
-		<h2>{id}</h2>
-	</div>
+	return (<>
+		<Head>
+			<title>{car.color} {car.id}</title>
+		</Head>
+
+		<h1>Hello {id}</h1>
+
+		<img src={car.image} alt="" width="300px"/>
+	</>)
+}
+
+// Fetching Data // On "Build"
+export async function getStaticProps({ params }) {
+	// Fetching the Json for an individual car // Getting the 'id' from the URL
+	const req = await fetch(`http://localhost:3000/${params.id}.json`)
+
+	// Converting to Json
+	const data = await req.json()
+
+	// Returning an object that has a 'props' property
+	// where each 'prop' can then be accessed by the component
+	return {
+		props: { car: data },
+	}
+}
+
+// Tells Next which dynamic pages to render
+export async function getStaticPaths({ params }) {
+	// Can also request data from API or Data-Base
+	const req = await fetch("http://localhost:3000/cars.json")
+
+	// Converting to Json
+	const data = await req.json()
+
+	// Returns paths object that contains an array with every route for this dynamic URL
+	const paths = data.map(car => {
+		return { params: { id: car }}
+	})
+
+	// Returning routes (tesla, ford, lambo) mapped to an array of objects
+	// Along with 'fallback: false' behavior (for 404 pages)
+	return {
+		paths,
+		fallback: false
+	}
 }
--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,3 +1,5 @@
+// Cars List Component
+
 export default function CarsList() {
 	return <h1>Cars List</h1>
 }
