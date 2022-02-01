732adb4d19f025416c68652251e0ff63a650325b
Simon Calle Laverde <kyverde88@gmail.com>
2022-02-01 14:49:20 -0500

    Updated "[id].js".
    - Returning back to "Static Site Generation" with 'getStaticProps' function and commenting the "Server-Side Rendering" code with 'getServerSideProps'.
    - Using back the 'getStaticPaths' function, which returns 'fallback: false' to bring a "404 Page" when accessing a none existent page, say: "http://localhost:3000/cars/deluxo".

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -19,45 +19,8 @@ export default function Car({ car }) {
 	</>)
 }
 
-// // Fetching Data // On "Build"
-// export async function getStaticProps({ params }) {
-// 	// Fetching the Json for an individual car // Getting the 'id' from the URL
-// 	const req = await fetch(`http://localhost:3000/${params.id}.json`)
-
-// 	// Converting to Json
-// 	const data = await req.json()
-
-// 	// Returning an object that has a 'props' property
-// 	// where each 'prop' can then be accessed by the component
-// 	return {
-// 		props: { car: data },
-// 	}
-// }
-
-// // Tells Next which dynamic pages to render
-// export async function getStaticPaths({ params }) {
-// 	// Can also request data from API or Data-Base
-// 	const req = await fetch("http://localhost:3000/cars.json")
-
-// 	// Converting to Json
-// 	const data = await req.json()
-
-// 	// Returns paths object that contains an array with every route for this dynamic URL
-// 	const paths = data.map(car => {
-// 		return { params: { id: car }}
-// 	})
-
-// 	// Returning routes (tesla, ford, lambo) mapped to an array of objects
-// 	// Along with 'fallback: false' behavior (for 404 pages)
-// 	return {
-// 		paths,
-// 		fallback: false
-// 	}
-// }
-
-// Fetching Data // At "Request Time" // (On Every Request)
-// Content is generated on a server when requested by the user
-export async function getServerSideProps({ params }) {
+// Fetching Data // On "Build"
+export async function getStaticProps({ params }) {
 	// Fetching the Json for an individual car // Getting the 'id' from the URL
 	const req = await fetch(`http://localhost:3000/${params.id}.json`)
 
@@ -70,3 +33,40 @@ export async function getServerSideProps({ params }) {
 		props: { car: data },
 	}
 }
+
+// Tells Next which dynamic pages to render
+export async function getStaticPaths({ params }) {
+	// Can also request data from API or Data-Base
+	const req = await fetch("http://localhost:3000/cars.json")
+
+	// Converting to Json
+	const data = await req.json()
+
+	// Returns paths object that contains an array with every route for this dynamic URL
+	const paths = data.map(car => {
+		return { params: { id: car }}
+	})
+
+	// Returning routes (tesla, ford, lambo) mapped to an array of objects
+	// Along with 'fallback: false' behavior (for 404 pages)
+	return {
+		paths,
+		fallback: false
+	}
+}
+
+// // Fetching Data // At "Request Time" // (On Every Request)
+// // Content is generated on a server when requested by the user
+// export async function getServerSideProps({ params }) {
+// 	// Fetching the Json for an individual car // Getting the 'id' from the URL
+// 	const req = await fetch(`http://localhost:3000/${params.id}.json`)
+
+// 	// Converting to Json
+// 	const data = await req.json()
+
+// 	// Returning an object that has a 'props' property
+// 	// where each 'prop' can then be accessed by the component
+// 	return {
+// 		props: { car: data },
+// 	}
+// }
